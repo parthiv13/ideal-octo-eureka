0e3b9687286708f79b3661cf7ccae4ed5598ab23
parthiv13 <kparthiv13@gmail.com>
2018-11-20 11:33:02 +0530

    Possible Event Emmitter Leaks

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs'),
+    path = require('path'),
+    csv = require('fast-csv');
 
-let scrape = async () => {
-    const browser = await puppeteer.launch( { headless: true });
+let scrape = async (email) => {
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
     await page.goto('http://mailtester.com/');
-    await page.type('input[name=email]', 'dan.conroy@aksteel.com', {delay: 20});
+    await page.type('input[name=email]', email, { delay: 20 });
     await page.click('input[type=submit]');
     await page.waitFor(1000);
 
@@ -21,6 +24,17 @@ let scrape = async () => {
     return result;
 };
 
-scrape().then((value) => {
+/*scrape().then((value) => {
     console.log(value);
-})
+})*/
+
+csv
+    .fromPath('leads.csv').on("data", function (data) {
+        if (data != "")
+            scrape(data).then((value) => {
+                console.log(value)
+            })
+    })
+    .on("end", function () {
+        console.log('done')
+    })
