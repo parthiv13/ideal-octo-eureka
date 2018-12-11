7ca66f709a94741075e885649973788383c62cae
parthiv13 <kparthiv13@gmail.com>
2018-12-11 15:09:43 +0530

    Changed headless status to true

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ macaddress.one(function (err, mac) {
                     console.log(arr);
                     (async () => {
                         try {
-                            const browser = await puppeteer.launch({ headless: false });
+                            const browser = await puppeteer.launch({ headless: true });
                             const page = await browser.newPage();
 
                             for (let email of arr) {
