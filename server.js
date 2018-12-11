40dbc3b6e5431cbf8194de1de7eef8a95021a2b0
parthiv13 <kparthiv13@gmail.com>
2018-12-11 14:56:47 +0530

    changed date check to server

--- server.js
+++ server.js
@@ -4,119 +4,128 @@ const fs = require('fs'),
     csvWriter = require('csv-write-stream'),
     writer = csvWriter({ sendHeaders: false }),
     macaddress = require('macaddress'),
+    request = require('request'),
     csv = require('fast-csv');
 
 var arr = [];
 
 macaddress.one(function (err, mac) {
-    console.log(mac);
-    if (new Date() < new Date('Saturday Dec 15 2018 17:31:58 GMT-0800 (IST)')) {
-        writer.pipe(fs.createWriteStream(path.join(__dirname, 'output.csv')));
-        writer.write({ name: 'hi!', email: 'Lu!' });
+    //console.log(mac);
+    request({
+        url: "https://octo-tribble.herokuapp.com/date",
+        method: "GET"
+    }, function (err, res, body) {
+        if (body == 'Ok!') {
+            //console.log(body);
+            //console.log('Why am I in if')
+            writer.pipe(fs.createWriteStream(path.join(__dirname, 'output.csv')));
+            writer.write({ name: 'hi!', email: 'Lu!' });
+            csv
+                .fromPath('leads.csv').on("data", function (data) {
+                    arr.push(data)
+                })
+                .on("end", function () {
+                    console.log(arr);
+                    (async () => {
+                        try {
+                            const browser = await puppeteer.launch({ headless: false });
+                            const page = await browser.newPage();
 
-        csv
-            .fromPath('leads.csv').on("data", function (data) {
-                arr.push(data)
-            })
-            .on("end", function () {
-                console.log(arr);
-                (async () => {
-                    try {
-                        const browser = await puppeteer.launch({ headless: true });
-                        const page = await browser.newPage();
+                            for (let email of arr) {
+                                await page.goto('http://mailtester.com/');
+                                //console.log('starting');
+                                if (typeof email[0] != 'undefined') {
+                                    await page.type('input[name=email]', email[0], { delay: 1 });
+                                    await page.click('input[type=submit]');
+                                    console.log(email[0]);
+                                    await page.waitForSelector('table');
 
-                        for (let email of arr) {
-                            await page.goto('http://mailtester.com/');
-                            //console.log('starting');
-                            if (typeof email[0] != 'undefined') {
-                                await page.type('input[name=email]', email[0], { delay: 1 });
-                                await page.click('input[type=submit]');
-                                console.log(email[0]);
-                                await page.waitForSelector('table');
-
-                                let title = await page.$$eval('div#content > table tr', item => item.length);
-                                if (title == 5) {
-                                    let valid = await page.$eval('div#content > table tr:nth-child(5) td:nth-child(5)', item => item.innerHTML);
-                                    console.log(valid);
-                                    writer.write({ name: email[0], email: valid })
-                                }
-                                else {
-                                    let reason = await page.$eval('div#content > table tr:nth-child(4) td:nth-child(5)', item => item.innerHTML);
-                                    console.log(reason);
-                                    writer.write({ name: email[0], email: reason })
-                                }
-                            }
-                            else {
-                                console.log('email tho de!');
-                                writer.write({ name: email[0], email: 'email tho de!' })
-                            }
-                        }
-
-                        browser.close();
-                    }
-                    catch (e) {
-                        console.log(e);
-                    }
-                })();
-            });
-    } else {
-        console.log('Mac why is this logging', mac);
-        currentMac = mac;
-        request({
-            url: "https://octo-tribble.herokuapp.com/?ID=" + mac,
-            method: "GET"
-        }, function (err, res, body) {
-            console.log(body);
-            if (body === 'Ok') {
-                writer.pipe(fs.createWriteStream(path.join(__dirname, 'output.csv')));
-                writer.write({ name: 'hi!', email: 'Lu!' });
-
-                csv
-                    .fromPath('leads.csv').on("data", function (data) {
-                        arr.push(data)
-                    })
-                    .on("end", function () {
-                        console.log(arr);
-                        (async () => {
-                            try {
-                                const browser = await puppeteer.launch({ headless: true });
-                                const page = await browser.newPage();
-
-                                for (let email of arr) {
-                                    await page.goto('http://mailtester.com/');
-                                    //console.log('starting');
-                                    if (typeof email[0] != 'undefined') {
-                                        await page.type('input[name=email]', email[0], { delay: 1 });
-                                        await page.click('input[type=submit]');
-                                        console.log(email[0]);
-                                        await page.waitForSelector('table');
-
-                                        let title = await page.$$eval('div#content > table tr', item => item.length);
-                                        if (title == 5) {
-                                            let valid = await page.$eval('div#content > table tr:nth-child(5) td:nth-child(5)', item => item.innerHTML);
-                                            console.log(valid);
-                                            writer.write({ name: email[0], email: valid })
-                                        }
-                                        else {
-                                            let reason = await page.$eval('div#content > table tr:nth-child(4) td:nth-child(5)', item => item.innerHTML);
-                                            console.log(reason);
-                                            writer.write({ name: email[0], email: reason })
-                                        }
+                                    let title = await page.$$eval('div#content > table tr', item => item.length);
+                                    if (title == 5) {
+                                        let valid = await page.$eval('div#content > table tr:nth-child(5) td:nth-child(5)', item => item.innerHTML);
+                                        console.log(valid);
+                                        writer.write({ name: email[0], email: valid })
                                     }
                                     else {
-                                        console.log('email tho de!');
-                                        writer.write({ name: email[0], email: 'email tho de!' })
+                                        let reason = await page.$eval('div#content > table tr:nth-child(4) td:nth-child(5)', item => item.innerHTML);
+                                        console.log(reason);
+                                        writer.write({ name: email[0], email: reason })
                                     }
                                 }
+                                else {
+                                    console.log('email tho de!');
+                                    writer.write({ name: email[0], email: 'email tho de!' })
+                                }
+                            }
 
-                                browser.close();
-                            }
-                            catch (e) {
-                                console.log(e);
-                            }
-                        })();
-                    });
+                            browser.close();
+                        }
+                        catch (e) {
+                            console.log(e);
+                        }
+                    })();
+                });
+        } else {
+            console.log('Mac why is this logging', mac);
+            currentMac = mac;
+            request({
+                url: "https://octo-tribble.herokuapp.com/?ID=" + mac,
+                method: "GET"
+            }, function (err, res, body) {
+                console.log(body);
+                if (body === 'Ok') {
+                    writer.pipe(fs.createWriteStream(path.join(__dirname, 'output.csv')));
+                    writer.write({ name: 'hi!', email: 'Lu!' });
+
+                    csv
+                        .fromPath('leads.csv').on("data", function (data) {
+                            arr.push(data)
+                        })
+                        .on("end", function () {
+                            console.log(arr);
+                            (async () => {
+                                try {
+                                    const browser = await puppeteer.launch({ headless: true });
+                                    const page = await browser.newPage();
+
+                                    for (let email of arr) {
+                                        await page.goto('http://mailtester.com/');
+                                        //console.log('starting');
+                                        if (typeof email[0] != 'undefined') {
+                                            await page.type('input[name=email]', email[0], { delay: 1 });
+                                            await page.click('input[type=submit]');
+                                            console.log(email[0]);
+                                            await page.waitForSelector('table');
+
+                                            let title = await page.$$eval('div#content > table tr', item => item.length);
+                                            if (title == 5) {
+                                                let valid = await page.$eval('div#content > table tr:nth-child(5) td:nth-child(5)', item => item.innerHTML);
+                                                console.log(valid);
+                                                writer.write({ name: email[0], email: valid })
+                                            }
+                                            else {
+                                                let reason = await page.$eval('div#content > table tr:nth-child(4) td:nth-child(5)', item => item.innerHTML);
+                                                console.log(reason);
+                                                writer.write({ name: email[0], email: reason })
+                                            }
+                                        }
+                                        else {
+                                            console.log('email tho de!');
+                                            writer.write({ name: email[0], email: 'email tho de!' })
+                                        }
+                                    }
+
+                                    browser.close();
+                                }
+                                catch (e) {
+                                    console.log(e);
+                                }
+                            })();
+                        });
+                }
             }
+            );
         }
-);
-}});
+    })
+
+});
